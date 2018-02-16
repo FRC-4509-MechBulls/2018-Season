a70e8debc821610799ef2fddf44b9d7b32348573
Kyle B <ltcnugget@gmail.com>
2018-02-16 09:51:28 -0500

    fixed formatting and corrected setting the interrupt in TurnCommand.

--- src/org/usfirst/frc/team4509/robot/commands/TurnCommand.java
+++ src/org/usfirst/frc/team4509/robot/commands/TurnCommand.java
@@ -1,9 +1,11 @@
 package org.usfirst.frc.team4509.robot.commands;
 
+
 import org.usfirst.frc.team4509.robot.Robot;
 import org.usfirst.frc.team4509.robot.RobotMap;
 
 import edu.wpi.first.wpilibj.command.Command;
+
 
 /**
  * A command to turn the robot an amount given in degrees 
@@ -13,35 +15,31 @@ import edu.wpi.first.wpilibj.command.Command;
 public class TurnCommand extends Command {
 
 	double turnDegrees, targetDegrees;
-	boolean isFinished = false;
 
-    public TurnCommand(double turnDegrees) {
-        requires(Robot.drivingSubsystem);
-        this.turnDegrees = turnDegrees > 180 ? turnDegrees - 360 : turnDegrees;
-        this.targetDegrees = this.turnDegrees + RobotMap.gyro.getAngle();
-    }
+	public TurnCommand(double turnDegrees) {
+		requires(Robot.drivingSubsystem);
+		this.setInterruptible(false);
+		this.turnDegrees = turnDegrees > 180 ? turnDegrees - 360 : turnDegrees;
+		this.targetDegrees = this.turnDegrees + RobotMap.gyro.getAngle();
+	}
 
-    protected void initialize() {  }
-    
-    protected void execute() {
-  		if(this.turnDegrees - RobotMap.gyro.getAngle() > 0)
-  			Robot.drivingSubsystem.turn(1);
-  		else
-  			Robot.drivingSubsystem.turn(-1);
-    }
+	protected void initialize() {
+		Robot.drivingSubsystem.stop();
+	}
 
-    protected boolean isFinished() {
-        return Math.abs(this.targetDegrees - RobotMap.gyro.getAngle()) < RobotMap.GYRO_PRECISION;
-    }
+	protected void execute() {
+		if(this.turnDegrees - RobotMap.gyro.getAngle() > 0)
+			Robot.drivingSubsystem.turn(1);
+		else
+			Robot.drivingSubsystem.turn(-1);
+	}
 
-    protected void end() {
-    	Robot.drivingSubsystem.stop();
-    }
+	protected boolean isFinished() {
+		return Math.abs(this.targetDegrees - RobotMap.gyro.getAngle()) < RobotMap.GYRO_PRECISION;
+	}
 
-    protected void interrupted() {  }
-    
-    protected boolean isInterruptable() {
-    	return false;
-    }
-    
+	protected void end() {
+		Robot.drivingSubsystem.stop();
+	}
+
 }
