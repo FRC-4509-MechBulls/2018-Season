e3096afc0f818535bf708b2f7a2c2a47a5e3d10d
Kyle B <ltcnugget@gmail.com>
2018-02-25 13:23:37 -0500

    set talons to coast to a stop.

--- src/org/usfirst/frc/team4509/robot/RobotMap.java
+++ src/org/usfirst/frc/team4509/robot/RobotMap.java
@@ -1,6 +1,7 @@
 package org.usfirst.frc.team4509.robot;
 
 
+import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
@@ -60,8 +61,11 @@ public class RobotMap {
 	public static void initDrive() {
 		// initialize the talons
 		RobotMap.leftDriveTalon   = new WPI_TalonSRX(RobotMap.LEFT_DRIVE_TALON_PORT);
+		RobotMap.leftDriveTalon.setNeutralMode(NeutralMode.Coast);
 		RobotMap.slideDriveTalon  = new WPI_TalonSRX(RobotMap.SLIDE_DRIVE_TALON_PORT);
+		RobotMap.leftDriveTalon.setNeutralMode(NeutralMode.Coast);
 		RobotMap.rightDriveTalon  = new WPI_TalonSRX(RobotMap.RIGHT_DRIVE_TALON_PORT);
+		RobotMap.leftDriveTalon.setNeutralMode(NeutralMode.Coast);
 		
 		RobotMap.drive = new DifferentialDrive(RobotMap.leftDriveTalon, RobotMap.rightDriveTalon);
 		
