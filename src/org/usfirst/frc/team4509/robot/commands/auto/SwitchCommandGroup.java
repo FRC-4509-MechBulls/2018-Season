c82062dfeb3ee39d172d9125cc82eae1dced5ff5
Andrew Goeden <andrewgoeden@gmail.com>
2018-03-02 08:31:57 -0500

    Commented the next steps for the SwitchCommandGroup

--- src/org/usfirst/frc/team4509/robot/commands/auto/SwitchCommandGroup.java
+++ src/org/usfirst/frc/team4509/robot/commands/auto/SwitchCommandGroup.java
@@ -13,14 +13,22 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class SwitchCommandGroup extends CommandGroup {
 
 	public SwitchCommandGroup() {
+		
+		// Check which side of the switch to score on
+		// Check which side we start on (right, center, left)
+		
 		addSequential(new DriveForFeetCommand(19.061));
 		addSequential(new TurnCommand(180 * Robot.startPosition));
 		addSequential(new SlideForSecondsCommand(-1, 2));
 		
+		// Acquire cube
 		
 		addSequential(new DriveForFeetCommand(1));
 		
+		// Raise the elevator to put it on the switch
+		
 		addSequential(new DriveForFeetCommand(1));
 		
+		// Drop the elevator.
 	}
 }
