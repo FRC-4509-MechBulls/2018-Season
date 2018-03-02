f4efdd29136e6637a5770787ba990fa6971812ed
Kyle B <ltcnugget@gmail.com>
2018-03-02 15:28:09 -0500

    removed slide command from basic auto.

--- src/org/usfirst/frc/team4509/robot/commands/auto/BasicCommandGroup.java
+++ src/org/usfirst/frc/team4509/robot/commands/auto/BasicCommandGroup.java
@@ -2,7 +2,7 @@ package org.usfirst.frc.team4509.robot.commands.auto;
 
 import org.usfirst.frc.team4509.robot.Robot;
 import org.usfirst.frc.team4509.robot.commands.DriveForFeetCommand;
-import org.usfirst.frc.team4509.robot.commands.SlideForSecondsCommand;
+import org.usfirst.frc.team4509.robot.commands.TurnCommand;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
@@ -14,7 +14,9 @@ public class BasicCommandGroup extends CommandGroup {
 
 	public BasicCommandGroup(int side) {
 		if(Robot.startPosition == 0) {
-			addSequential(new SlideForSecondsCommand(1 * side, 2));
+			addSequential(new TurnCommand(90 * side));
+			addSequential(new DriveForFeetCommand(5.5));
+			addSequential(new TurnCommand(-90 * side));
 			addSequential(new DriveForFeetCommand(13.667));
 		} else {
 			addSequential(new DriveForFeetCommand(19.061));
