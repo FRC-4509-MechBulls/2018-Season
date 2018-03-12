0f85c9f28cd42c5284e0436bfa1ee9ba5db2bd89
Kyle B <ltcnugget@gmail.com>
2018-03-12 09:57:15 -0400

    added more logging to DriveForFeetTestCommand. moved TurnTestCommand.

--- src/org/usfirst/frc/team4509/robot/commands/test/DriveForFeetTestCommand.java
+++ src/org/usfirst/frc/team4509/robot/commands/test/DriveForFeetTestCommand.java
@@ -16,22 +16,22 @@ public class DriveForFeetTestCommand extends Command {
 		double distance = 1;
 		this.distance = this.distance * (12 * Preferences.getInstance().getInt("TICKS_PER_INCH", RobotMap.TICKS_PER_INCH));
 		System.out.println(distance + " feet = " + this.distance + " ticks.");
-		this.direction = 1;
+		this.direction = (int)(this.distance / Math.abs(this.distance));
+		System.out.println("We are going " + (this.direction > 0 ? "forward" : "backward") + ".");
 	}
 
 	protected void initialize() {
 		Robot.drivingSubsystem.resetEncoders();
-		System.out.println(this.distance);
-		this.direction = (int)(this.distance / Math.abs(this.distance));
-		System.out.println("We are going " + (this.direction > 0 ? "forward" : "backward") + ".");
+		System.out.println("Encoders are at " + Robot.drivingSubsystem.getAverageEncoderTicks() + ".");
 	} 
 
 	protected void execute() {
 		System.out.println("Driving at speed " + (this.distance / Math.abs(this.distance)) + ".");
-		Robot.drivingSubsystem.drive(this.distance / Math.abs(this.distance));
+		Robot.drivingSubsystem.drive(this.direction);
 	}
 
 	protected boolean isFinished() {
+		System.out.println("The encoder says we are at " + Robot.drivingSubsystem.getAverageEncoderTicks() + " ticks");
 		if(direction == 1) {
 			System.out.println("We still have " + (this.distance - Robot.drivingSubsystem.getAverageEncoderTicks()) + " ticks to drive.");
 			return this.distance - Robot.drivingSubsystem.getAverageEncoderTicks() < 0.5;
